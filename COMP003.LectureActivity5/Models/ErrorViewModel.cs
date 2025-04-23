a37f73ca6ae32eff3363890a77fed4b68d87dd92
romanvillagran <romanvillagran386@gmail.com>
2025-04-22 23:04:01 -0700

    Add project files.

---
+++ COMP003.LectureActivity5/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003.LectureActivity5.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
