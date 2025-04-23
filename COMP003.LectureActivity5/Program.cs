63d20da879355265caf8c3573efcea41fbaa1159
romanvillagran <romanvillagran386@gmail.com>
2025-04-23 14:06:34 -0700

    had to fix some stuff

--- COMP003.LectureActivity5/Program.cs
+++ COMP003.LectureActivity5/Program.cs
@@ -5,12 +5,22 @@ namespace COMP003.LectureActivity5
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+           
 
-            builder.Services.AddControllers();
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(); 
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
             if (app.Environment.IsDevelopment())
             {
@@ -19,8 +29,15 @@ namespace COMP003.LectureActivity5
             }
 
             app.UseHttpsRedirection();
+            app.UseRouting();
+
             app.UseAuthorization();
-            app.MapControllers();
+
+            app.MapStaticAssets();
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+                .WithStaticAssets();
 
             app.Run();
         }
